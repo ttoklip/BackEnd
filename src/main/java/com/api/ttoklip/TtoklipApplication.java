9bb2ba899230c220f4e20907963df9bfa8486c02
toychip <dlawnsgud427@naver.com>
2024-01-03 22:50:40 +0900

    Initial commit

---
+++ src/main/java/com/api/ttoklip/TtoklipApplication.java
@@ -0,0 +1,13 @@
+package com.api.ttoklip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtoklipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtoklipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ttoklip/TtoklipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ttoklip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtoklipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
