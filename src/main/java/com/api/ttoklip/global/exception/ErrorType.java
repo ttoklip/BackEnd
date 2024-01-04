6b9d5086149a73d2ee01b4447c6ec5a19bb14db4
toychip <dlawnsgud427@naver.com>
2024-01-04 16:38:52 +0900

    feat: S3FileUploader (#1)
    S3 bucket에 업로드하는 Uploader 추가

--- src/main/java/com/api/ttoklip/global/exception/ErrorType.java
+++ src/main/java/com/api/ttoklip/global/exception/ErrorType.java
@@ -1,5 +1,8 @@
 package com.api.ttoklip.global.exception;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,14 @@ public enum ErrorType {
      * message : 사람이 알아볼 수 있도록 작성
      * ex) "인증이 필요합니다."
      */
+
+
+    // ------------------------------------------ S3 ------------------------------------------
+    EXCEEDING_FILE_COUNT(BAD_REQUEST, "S4001", "사진 개수가 너무 많습니다."),
+    EXCEEDING_FILE_SIZE(BAD_REQUEST, "S4002", "사진 용량이 너무 큽니다."),
+    S3_UPLOAD(INTERNAL_SERVER_ERROR, "S5001", "서버오류, S3 사진 업로드 에러입니다."),
+    S3_CONNECT(INTERNAL_SERVER_ERROR, "S5002", "서버오류, S3 연결 에러입니다."),
+    S3_CONVERT(INTERNAL_SERVER_ERROR, "S5003", "서버오류, S3 변환 에러입니다."),
 
     ;
 
