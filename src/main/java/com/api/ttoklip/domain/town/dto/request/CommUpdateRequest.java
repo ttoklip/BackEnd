ded659880d1f46403f7e4bc6be945916af5eb920
jgmoon <6813821@tukorea.ac.kr>
2024-01-12 14:44:20 +0900

    refactor: 소통해요 글 API 수정 (#8)
    함께해요, 소통해요 controller 분리 필요

---
+++ src/main/java/com/api/ttoklip/domain/town/dto/request/CommUpdateRequest.java
@@ -0,0 +1,4 @@
+package com.api.ttoklip.domain.town.dto.request;
+
+public class CommUpdateRequest {
+}
---
+++ src/main/java/com/api/ttoklip/domain/town/dto/response/CommResponse.java
@@ -0,0 +1,4 @@
+package com.api.ttoklip.domain.town.dto.response;
+
+public class CommResponse {
+}
