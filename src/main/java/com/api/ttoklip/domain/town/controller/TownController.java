921cf7fa879bc5d24717bc9626e86520efc0ee9f
jgmoon <6813821@tukorea.ac.kr>
2024-01-12 13:48:28 +0900

    refactor: 함께해요 글 API 수정 (#8)
    CartCreateRequest CartUpdateRequest

--- src/main/java/com/api/ttoklip/domain/town/controller/TownController.java
+++ src/main/java/com/api/ttoklip/domain/town/controller/TownController.java
@@ -1,7 +1,10 @@
 package com.api.ttoklip.domain.town.controller;
 
+import com.api.ttoklip.domain.town.dto.request.CartCreateRequest;
 import com.api.ttoklip.domain.town.dto.request.CartSearchCondition;
+import com.api.ttoklip.domain.town.dto.request.CartUpdateRequest;
 import com.api.ttoklip.domain.town.dto.response.CartListResponse;
+import com.api.ttoklip.domain.town.dto.response.CartResponse;
 import com.api.ttoklip.domain.town.service.CartService;
 import com.api.ttoklip.domain.town.service.CommService;
 import com.api.ttoklip.global.success.SuccessResponse;
@@ -28,53 +31,67 @@ public class TownController {
     private final CartService cartService;
     private final CommService commService;
 
+    // 함께해요 - cart
     @Operation(summary = "함께해요 더보기 페이지 조회",
             description = "함께해요 더보기 페이지를 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "함께해요 더보기 페이지 조회 성공",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = CartListResponse.class))),
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = CartListResponse.class))),
             @ApiResponse(responseCode = "400", description = "함께해요 더보기 페이지 조회 실패"),
     })
     @GetMapping("/carts")
     public SuccessResponse<CartListResponse> getCartPage(final @Validated @ModelAttribute CartSearchCondition condition,
                                                          final Pageable pageable) {
-        CartListResponse cartListResponse = CartService.searchCartPaging(condition, pageable);
+        CartListResponse cartListResponse = cartService.searchCartPaging(condition, pageable);
         return new SuccessResponse<>(cartListResponse);
     }
 
     @Operation(summary = "함께해요 게시글 조회",
             description = "함께해요 단일 게시글을 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "함께해요 게시글 조회 성공"),
+            @ApiResponse(responseCode = "200", description = "함께해요 게시글 조회 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = CartResponse.class))),
             @ApiResponse(responseCode = "400", description = "함께해요 게시글 조회 실패"),
     })
     @GetMapping("/carts/{cartId}")
-    public ResponseEntity<?> getCartPost() {
-        return null;
+    public SuccessResponse<CartResponse> getCart(final @PathVariable Long cartId) {
+        CartResponse cartResponse = cartService.getCart(cartId);
+        return new SuccessResponse<>(cartResponse);
     }
 
     @Operation(summary = "함께해요 게시글 생성",
             description = "함께해요 게시글을 생성합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "함께해요 게시글 생성 성공"),
+            @ApiResponse(responseCode = "200", description = "함께해요 게시글 생성 성공",
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
+                        schema = @Schema(implementation = CartResponse.class))),
             @ApiResponse(responseCode = "400", description = "함께해요 게시글 생성 실패"),
     })
-    @PostMapping("/carts/{cartId}")
-    public ResponseEntity<?> createCartPost() { return null; }
+    @PostMapping("/carts")
+    public SuccessResponse<CartResponse> createCartPost(final @Validated @ModelAttribute CartCreateRequest request) {
+        CartResponse cartResponse = cartService.createCartPost(request);
+        return new SuccessResponse<>(cartResponse);
+    }
 
     // 함께해요는 글 삭제 불가능, 수정만 ok
     @Operation(summary = "함께해요 게시글 수정",
             description = "함께해요 게시글을 수정합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "함께해요 게시글 수정 성공"),
+            @ApiResponse(responseCode = "200", description = "함께해요 게시글 수정 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "400", description = "함께해요 게시글 수정 실패"),
     })
     @PatchMapping("/carts/{cartId}")
-    public ResponseEntity<?> updateCartPost() {
-        return null;
+    public SuccessResponse<Long> updateCartPost(final @PathVariable Long cartId,
+                                                final @ModelAttribute CartUpdateRequest request) {
+        cartService.updateCartPost(cartId, request);
+        return new SuccessResponse<>(cartId);
     }
 
+    // 소통해요 - comm
     @Operation(summary = "소통해요 더보기 페이지 조회",
             description = "소통해요 더보기 페이지를 조회합니다.")
     @ApiResponses(value = {
--- src/main/java/com/api/ttoklip/domain/town/service/CartService.java
+++ src/main/java/com/api/ttoklip/domain/town/service/CartService.java
@@ -1,7 +1,10 @@
 package com.api.ttoklip.domain.town.service;
 
+import com.api.ttoklip.domain.town.dto.request.CartCreateRequest;
 import com.api.ttoklip.domain.town.dto.request.CartSearchCondition;
+import com.api.ttoklip.domain.town.dto.request.CartUpdateRequest;
 import com.api.ttoklip.domain.town.dto.response.CartListResponse;
+import com.api.ttoklip.domain.town.dto.response.CartResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +17,17 @@ public class CartService {
     // 더보기 페이지 조회
     public static CartListResponse searchCartPaging(CartSearchCondition condition, Pageable pageable) {
         return null;
+    }
+
+    public CartResponse getCart(Long cartId) {
+        return null;
+    }
+
+    public CartResponse createCartPost(CartCreateRequest request) {
+        return null;
+    }
+
+    public void updateCartPost(Long cartId, CartUpdateRequest request) {
+        return;
     }
 }
