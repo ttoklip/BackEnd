b92230ba8d8b00341e2a430d1b6b944e56601218
jgmoon <6813821@tukorea.ac.kr>
2024-01-12 14:43:24 +0900

    refactor: 소통해요 더보기 페이지 조회 API 수정 (#8)
    CommListResponse CommSearchCondition CommService

--- src/main/java/com/api/ttoklip/domain/town/controller/TownController.java
+++ src/main/java/com/api/ttoklip/domain/town/controller/TownController.java
@@ -1,10 +1,10 @@
 package com.api.ttoklip.domain.town.controller;
 
-import com.api.ttoklip.domain.town.dto.request.CartCreateRequest;
-import com.api.ttoklip.domain.town.dto.request.CartSearchCondition;
-import com.api.ttoklip.domain.town.dto.request.CartUpdateRequest;
+import com.api.ttoklip.domain.town.dto.request.*;
 import com.api.ttoklip.domain.town.dto.response.CartListResponse;
 import com.api.ttoklip.domain.town.dto.response.CartResponse;
+import com.api.ttoklip.domain.town.dto.response.CommListResponse;
+import com.api.ttoklip.domain.town.dto.response.CommResponse;
 import com.api.ttoklip.domain.town.service.CartService;
 import com.api.ttoklip.domain.town.service.CommService;
 import com.api.ttoklip.global.success.SuccessResponse;
@@ -17,6 +17,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -95,54 +96,73 @@ public class TownController {
     @Operation(summary = "소통해요 더보기 페이지 조회",
             description = "소통해요 더보기 페이지를 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "소통해요 더보기 페이지 조회 성공"),
+            @ApiResponse(responseCode = "200", description = "소통해요 더보기 페이지 조회 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = CommListResponse.class))),
             @ApiResponse(responseCode = "400", description = "소통해요 더보기 페이지 조회 실패"),
     })
     @GetMapping("/comms")
-    public ResponseEntity<?> getCommsPage() {
-        return null;
+    public SuccessResponse<CommListResponse> getCommPage(final @Validated @ModelAttribute CommSearchCondition condition,
+                                                         final Pageable pageable) {
+        CommListResponse commListResponse = commService.searchCommPaging(condition, pageable);
+        return new SuccessResponse<>(commListResponse);
     }
 
     @Operation(summary = "소통해요 게시글 조회",
             description = "소통해요 단일 게시글을 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "소통해요 게시글 조회 성공"),
+            @ApiResponse(responseCode = "200", description = "소통해요 게시글 조회 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = CommResponse.class))),
             @ApiResponse(responseCode = "400", description = "소통해요 게시글 조회 실패"),
     })
-    @GetMapping("/comms/{commsId}")
-    public ResponseEntity<?> getCommsPost() {
-        return null;
+    @GetMapping("/comms/{commId}")
+    public SuccessResponse<CommResponse> getComm(final @PathVariable Long commId) {
+        CommResponse commResponse = commService.getComm(commId);
+        return new SuccessResponse<>(commResponse);
     }
 
     @Operation(summary = "소통해요 게시글 생성",
             description = "소통해요 게시글을 생성합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "소통해요 게시글 생성 성공"),
+            @ApiResponse(responseCode = "200", description = "소통해요 게시글 생성 성공",
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
+                            schema = @Schema(implementation = CommResponse.class))),
             @ApiResponse(responseCode = "400", description = "소통해요 게시글 생성 실패"),
     })
-    @PostMapping("/comms/{commsId}")
-    public ResponseEntity<?> createCommsPost() { return null; }
+    @PostMapping("/comms")
+    public SuccessResponse<CommResponse> createCommPost(final @Validated @ModelAttribute CommCreateRequest request) {
+        CommResponse commResponse = commService.createCommPost(request);
+        return new SuccessResponse<>(commResponse);
+    }
 
     @Operation(summary = "소통해요 게시글 수정",
             description = "소통해요 게시글을 수정합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "소통해요 게시글 수정 성공"),
+            @ApiResponse(responseCode = "200", description = "소통해요 게시글 수정 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "400", description = "소통해요 게시글 수정 실패"),
     })
-    @PatchMapping("/comms/{commsId}")
-    public ResponseEntity<?> updateCommsPost() {
-        return null;
+    @PatchMapping("/comms/{commId}")
+    public SuccessResponse<Long> updateCommPost(final @PathVariable Long commId,
+                                                final @ModelAttribute CommUpdateRequest request) {
+        commService.updateCommPost(commId, request);
+        return new SuccessResponse<>(commId);
     }
 
     @Operation(summary = "소통해요 게시글 삭제",
             description = "소통해요 게시글을 삭제합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "소통해요 게시글 삭제 성공"),
+            @ApiResponse(responseCode = "200", description = "소통해요 게시글 삭제 성공",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "400", description = "소통해요 게시글 삭제 실패"),
     })
-    @DeleteMapping("/comms/{commsId}")
-    public ResponseEntity<?> deleteCommsPost() {
-        return null;
+    @DeleteMapping("/comms/{commId}")
+    public SuccessResponse<CommResponse> deleteCommPost(final @PathVariable Long commId) {
+        commService.deleteCommPost(commId);
+        return new SuccessResponse<>(commId);
     }
 
 }
--- src/main/java/com/api/ttoklip/domain/town/service/CommService.java
+++ src/main/java/com/api/ttoklip/domain/town/service/CommService.java
@@ -1,9 +1,35 @@
 package com.api.ttoklip.domain.town.service;
 
+import com.api.ttoklip.domain.town.dto.request.CommCreateRequest;
+import com.api.ttoklip.domain.town.dto.request.CommSearchCondition;
+import com.api.ttoklip.domain.town.dto.request.CommUpdateRequest;
+import com.api.ttoklip.domain.town.dto.response.CommListResponse;
+import com.api.ttoklip.domain.town.dto.response.CommResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
 
 @Service
 @RequiredArgsConstructor
 public class CommService {
+    public CommListResponse searchCommPaging(CommSearchCondition condition, Pageable pageable) {
+        return null;
+    }
+
+    public CommResponse getComm(Long commId) {
+        return null;
+    }
+
+    public CommResponse createCommPost(CommCreateRequest request) {
+        return null;
+    }
+
+    public void updateCommPost(Long commId, CommUpdateRequest request) {
+        return;
+    }
+
+    public void deleteCommPost(Long commId) {
+        return;
+    }
 }
