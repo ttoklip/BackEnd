1236fff7b0f28db9c8ed180c3d5f8d8bda2958df
jgmoon <6813821@tukorea.ac.kr>
2024-01-25 17:12:34 +0900

    comment: 이미지 엔티티 주석 (#8)

--- src/main/java/com/api/ttoklip/domain/town/community/image/Image.java
+++ src/main/java/com/api/ttoklip/domain/town/community/image/Image.java
@@ -1,20 +1,20 @@
-package com.api.ttoklip.domain.town.community.image;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@NoArgsConstructor
-public class Image {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String filePath;
-}
+//package com.api.ttoklip.domain.town.community.image;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//public class Image {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String filePath;
+//}
