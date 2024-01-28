2ba5a7708b51c560a5ee77708263d6b8082acb0e
toychip <dlawnsgud427@naver.com>
2024-01-28 17:10:52 +0900

    refactor: 중복된 카테고리 공통 도메인으로 통합 (#22)

--- src/main/java/com/api/ttoklip/domain/common/Category.java
+++ src/main/java/com/api/ttoklip/domain/common/Category.java
@@ -1,18 +1,19 @@
-package com.api.ttoklip.domain.SignUp.main.domain;
+package com.api.ttoklip.domain.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public enum Category {
+
     HOUSEWORK("집안일", 1),
     RECIPE("레시피", 2),
     SAFE_LIVING("안전한 생활", 3),
     WELFARE_POLICY("복지 정책", 4);
 
-
     private final String name;
     private final int code;
+
 
 }
