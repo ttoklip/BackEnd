136530d5e1915205a4186f2a1e476052018bc4ab
toychip <dlawnsgud427@naver.com>
2024-01-28 17:13:16 +0900

    feat: 이름과 코드로 카테고리 찾는 메서드 추가 (#22)

--- src/main/java/com/api/ttoklip/domain/common/Category.java
+++ src/main/java/com/api/ttoklip/domain/common/Category.java
@@ -1,5 +1,7 @@
 package com.api.ttoklip.domain.common;
 
+import com.api.ttoklip.global.exception.ApiException;
+import com.api.ttoklip.global.exception.ErrorType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -15,5 +17,21 @@ public enum Category {
     private final String name;
     private final int code;
 
+    public static Category findCategoryByName(final String name) {
+        for (Category category : Category.values()) {
+            if (category.getName().equals(name)) {
+                return category;
+            }
+        }
+        throw new ApiException(ErrorType.CATEGORY_NOT_FOUNT);
+    }
 
+    public static Category findCategoryByCode(final int code) {
+        for (Category category : Category.values()) {
+            if (category.getCode() == code) {
+                return category;
+            }
+        }
+        throw new ApiException(ErrorType.CATEGORY_NOT_FOUNT);
+    }
 }
