98e02f7658553b238c863e1146ae4e1311d38df0
LeeJaeHyeok97 <hazardous10@naver.com>
2024-01-30 16:39:37 +0900

    fix: Messaage.class -> Message.class 오타 수정

--- src/main/java/com/api/ttoklip/global/success/Message.java
+++ src/main/java/com/api/ttoklip/global/success/Message.java
@@ -7,16 +7,16 @@ import lombok.ToString;
 
 @ToString
 @Data
-public class Messaage {
+public class Message {
 
     @Schema(type = "string", example = "메시지 문구를 출력합니다.", description = "메시지 입니다.")
     private String message;
 
-    public Messaage() {
+    public Message() {
     }
 
     @Builder
-    public Messaage(String message) {
+    public Message(String message) {
         this.message = message;
     }
 
