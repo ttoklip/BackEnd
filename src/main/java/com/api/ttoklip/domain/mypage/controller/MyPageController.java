d6d2afb244d604a9aec2ea6eaa4714cfc76d3768
SaeJoon <psae0714@naver.com>
2024-02-01 21:17:08 +0900

    docs:#4 api응답 예시 작성 constant

---
+++ src/main/java/com/api/ttoklip/domain/mypage/controller/MyPageController.java
@@ -0,0 +1,4 @@
+package com.api.ttoklip.domain.mypage.controller;
+
+public class MyPageController {
+}
---
+++ src/main/java/com/api/ttoklip/domain/mypage/service/MyPageService.java
@@ -0,0 +1,4 @@
+package com.api.ttoklip.domain.mypage.service;
+
+public class MyPageService {
+}
