c9e2e7e6f0348398ecdf699121020924245be947
LeeJaeHyeok97 <hazardous10@naver.com>
2024-02-02 18:08:48 +0900

    feat: CurrentUser 커스텀 애노테이션 추가

---
+++ src/main/java/com/api/ttoklip/global/config/security/token/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.api.ttoklip.global.config.security.token;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
