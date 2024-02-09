c03fd04c7bfcefdde1bc0c11e0b430a92e5dcd45
jgmoon <6813821@tukorea.ac.kr>
2024-02-09 21:56:37 +0900

    feat: Scrap 엔티티 추가 (#42)

---
+++ src/main/java/com/api/ttoklip/domain/town/community/scrap/entity/Scrap.java
@@ -0,0 +1,32 @@
+package com.api.ttoklip.domain.town.community.scrap.entity;
+
+import com.api.ttoklip.domain.Member;
+import com.api.ttoklip.domain.town.community.post.entity.Community;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Scrap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_id")
+    private Community community;
+
+    @Builder
+    public Scrap(Member member, Community community) {
+        this.member = member;
+        this.community = community;
+    }
+}
