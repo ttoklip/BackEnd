8e69a190bd500f3ec2cb42fa51f133b5e1f3aa95
toychip <dlawnsgud427@naver.com>
2024-02-13 04:46:42 +0900

    fix: 개인정보 기입, 로그인 명세서 작성 (#51)

--- src/main/java/com/api/ttoklip/global/security/auth/controller/AuthController.java
+++ src/main/java/com/api/ttoklip/global/security/auth/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.api.ttoklip.global.security.auth.controller;
 
+import com.api.ttoklip.domain.privacy.constant.PrivacyConstant;
 import com.api.ttoklip.global.security.auth.dto.LoginRequest;
 import com.api.ttoklip.global.security.auth.dto.LoginResponse;
 import com.api.ttoklip.global.security.auth.service.AuthService;
 import com.api.ttoklip.global.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "Server 자체 로그인", description = "oauth accessToken으로 로그인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "oauth accessToken으로 로그인",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = SuccessResponse.class),
+                            examples = @ExampleObject(
+                                    name = "SuccessResponse",
+                                    value = PrivacyConstant.LOGIN_SUCCESS,
+                                    description = "로그인"
+                            )))})
     @PostMapping
     public SuccessResponse<LoginResponse> login(final @RequestBody LoginRequest request) {
         LoginResponse loginResponse = authService.authenticate(request);
