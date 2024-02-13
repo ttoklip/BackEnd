648bf3072cc2a6791cb5c29a62aaae0b1ade5a0f
SaeJoon <psae0714@naver.com>
2024-02-13 23:18:43 +0900

    fix: 최종 pr 리뷰 반영(#45)

--- src/main/java/com/api/ttoklip/domain/mypage/noti/post/domain/NoticeRepository.java
+++ src/main/java/com/api/ttoklip/domain/mypage/noti/post/domain/NoticeRepository.java
@@ -1,12 +1,7 @@
 package com.api.ttoklip.domain.mypage.noti.post.domain;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeRepositoryCustom {
-    @Query(value = "SELECT n FROM Notice n")
-    Page<Notice> findByContentContaining(final Pageable pageable);
-
 }
