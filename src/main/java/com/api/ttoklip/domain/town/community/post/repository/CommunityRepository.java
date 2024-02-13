5bff18b757eeb15e833d3d4c5802a105904677f7
jgmoon <6813821@tukorea.ac.kr>
2024-02-14 00:49:44 +0900

    fix: 미사용 import 정리 (#42)

--- src/main/java/com/api/ttoklip/domain/town/community/post/repository/CommunityRepository.java
+++ src/main/java/com/api/ttoklip/domain/town/community/post/repository/CommunityRepository.java
@@ -1,13 +1,8 @@
 package com.api.ttoklip.domain.town.community.post.repository;
 
-import com.api.ttoklip.domain.town.community.like.entity.CommunityLike;
 import com.api.ttoklip.domain.town.community.post.entity.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CommunityRepository extends JpaRepository<Community, Long>, CommunityRepositoryCustom {
-//    Optional<CommunityLike> findByCommunityIdAndMemberId(Long communityId, Long memberId);
 
-    boolean existsByCommunityIdAndMemberId(Long postId, Long memberId);
 }
