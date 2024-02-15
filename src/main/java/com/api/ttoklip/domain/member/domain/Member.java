7faf2fefc812e860badae110f61d073965225254
jgmoon <6813821@tukorea.ac.kr>
2024-02-15 18:09:49 +0900

    feat: 멤버 - 뉴스레터 OneToMany 매핑 (#67)

--- src/main/java/com/api/ttoklip/domain/member/domain/Member.java
+++ src/main/java/com/api/ttoklip/domain/member/domain/Member.java
@@ -9,6 +9,7 @@ import com.api.ttoklip.domain.honeytip.post.domain.HoneyTip;
 import com.api.ttoklip.domain.member.editor.MemberEditor;
 import com.api.ttoklip.domain.member.editor.MemberEditor.MemberEditorBuilder;
 import com.api.ttoklip.domain.newsletter.like.entity.NewsletterLike;
+import com.api.ttoklip.domain.newsletter.post.domain.Newsletter;
 import com.api.ttoklip.domain.newsletter.scarp.entity.NewsletterScrap;
 import com.api.ttoklip.domain.privacy.domain.Interest;
 import com.api.ttoklip.domain.privacy.domain.Profile;
@@ -93,6 +94,10 @@ public class Member extends BaseEntity {
     @Builder.Default
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CommunityScrap> communityScraps = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Newsletter> newsletters = new ArrayList<>();
 
     @Builder.Default
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
