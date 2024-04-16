58c744fadfeffcd36e4c892566f7bfa17925d208
toychip <dlawnsgud427@naver.com>
2024-04-16 17:28:05 +0900

    feat: FCMManager를 사용하여 푸쉬 알림 전송 (#126)

--- src/main/java/com/api/ttoklip/domain/notification/entity/NotiCategory.java
+++ src/main/java/com/api/ttoklip/domain/notification/entity/NotiCategory.java
@@ -7,24 +7,24 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum NotiCategory {
 
-    HONEY_TIP_SCRAP("꿀팁 공유해요 스크랩"),
-    HONEY_TIP_HELPFUL("꿀팁 공유해요 도움이 되었어요"),
-    HONEY_TIP_COMMENT("꿀팁 공유해요 댓글"),
-    HONEY_TIP_CHILD_COMMENT("꿀팁 공유해요 답글"),
+    HONEY_TIP_SCRAP("꿀팁 공유해요", "누군가가 글을 스크랩 했어요"),
+    HONEY_TIP_HELPFUL("꿀팁 공유해요", "누군가에게 글이 도움이 되었대요"),
+    HONEY_TIP_COMMENT("꿀팁 공유해요", "누군가가 댓글을 남겼어요"),
+    HONEY_TIP_CHILD_COMMENT("꿀팁 공유해요", "누군가 답글을 남겼어요"),
 
-    QUESTION_HELPFUL("질문해요 도움이 되었어요"),
-    QUESTION_COMMENT("질문해요 댓글"),
-    QUESTION_CHILD_COMMENT("질문해요 답글"),
+    QUESTION_HELPFUL("질문해요", "누군가에게 남겨준 댓글이 도움이 되었대요"),
+    QUESTION_COMMENT("질문해요", "누군가가 댓글을 남겼어요"),
+    QUESTION_CHILD_COMMENT("질문해요", "누군가가 답글을 남겼어요"),
 
-    NEWS_LETTER_CHILD_COMMENT("뉴스레터 답글"),
+    NEWS_LETTER_CHILD_COMMENT("뉴스레터", "누군가가 답글을 남겼어요"),
 
-    OUR_TOWN_SCRAP("우리동네 스크랩"),
-    OUR_TOWN_COMMENT("우리동네 댓글"),
-    OUR_TOWN_CHILD_COMMENT("우리동네 답글"),
-    OUR_TOWN_TOGETHER("우리동네 함께해요"),
+    OUR_TOWN_SCRAP("우리동네 스크랩", "누군가가 글을 스크랩 했어요"),
+    OUR_TOWN_COMMENT("우리동네 댓글", "누군가가 댓글을 남겼어요"),
+    OUR_TOWN_CHILD_COMMENT("우리동네 답글", "누군가가 답글을 남겼어요"),
+    OUR_TOWN_TOGETHER("우리동네 함께해요", "누군가가 함께하기에 참여했어요"),
 
-    PROFILE_LIKE("프로필 좋아요")
-    ;
+    PROFILE_LIKE("프로필 좋아요", "누군가가 내 프로필에 좋아요를 눌렀어요");
 
-    private final String name;
+    private final String title;
+    private final String text;
 }
---
+++ src/main/java/com/api/ttoklip/domain/notification/service/FCMManager.java
@@ -0,0 +1,36 @@
+package com.api.ttoklip.domain.notification.service;
+
+import com.api.ttoklip.domain.notification.entity.NotiCategory;
+import com.api.ttoklip.global.exception.ApiException;
+import com.api.ttoklip.global.exception.ErrorType;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FCMManager {
+
+    private static final String TEXT_DATA_NAME = "text";
+    private static final String TITLE_DATA_NAME = "title";
+
+    public void send(
+            final NotiCategory categoryName,
+            final String fcmToken
+    ) {
+        try {
+            FirebaseMessaging.getInstance()
+                    .send(
+                            Message.builder()
+                                    .putData(TITLE_DATA_NAME, categoryName.getTitle())
+                                    .putData(TEXT_DATA_NAME, categoryName.getText())
+                                    .setToken(fcmToken)
+                                    .build()
+                    );
+        } catch (FirebaseMessagingException e) {
+            throw new ApiException(ErrorType._NOT_SEND_ABLE);
+        }
+    }
+}
