b68ccd2bcaf0b3c453fe913361f8a38c03309a5c
toychip <dlawnsgud427@naver.com>
2024-04-16 23:59:49 +0900

    feat: 답글 생성시 댓글글 작성자와 현재 사용자가 일치하는지 여부 값 추가 (#126)

--- src/main/java/com/api/ttoklip/domain/notification/service/NotificationDispatcher.java
+++ src/main/java/com/api/ttoklip/domain/notification/service/NotificationDispatcher.java
@@ -28,40 +28,37 @@ public class NotificationDispatcher {
     private final NotificationRegistry notificationRegistry;
 
     public void dispatchNotification(final NotificationRequest request) {
-        NotiCategory notiCategory = notificationRegistry.determineNotiCategory(request.className(), request.methodName());
+        NotiCategory notiCategory = notificationRegistry.determineNotiCategory(request.className(),
+                request.methodName());
         dispatch(request.targetMemberId(), notiCategory);
     }
 
     // 댓글 알림 타겟 결정 및 알림 전송
     public void dispatchCommentNotification(final Long targetMemberId, final Comment comment) {
         NotiCategory notiCategory = notificationRegistry.determineCommentNotiCategory(comment);
+        if (notiCategory.equals(NotiCategory.BAD_TYPE_NOTIFICATION)) {
+            log.info("[NOTICE BAD TYPE] 잘못된 로그가 반환되어 알림 AOP를 종료합니다.");
+            return;
+        }
 
         // 게시글 작성자에게 알릴 여부
         boolean notifyPostWriter = notiCategory.isNotifyPostWriter();
         // 댓글 작성자에게 알릴 여부
         boolean notifyCommentWriter = notiCategory.isNotifyCommentWriter();
 
-        // 댓글이 생성됨을 게시글 작성자에게 알려야하는데 현재 사용자가 게시글 작성자가 아니라면
+        // 댓글이 생성됨을 게시글 작성자에게 알려야하는데 현재 사용자가 게시글 작성자가 맞다면
         if (notifyPostWriter) {
-            if (!determinePostNoticeTarget(comment)) {
-                return;
+            if (determinePostNoticeTarget(comment)) {
+                dispatch(targetMemberId, notiCategory);
             }
         }
 
-        // 답글이 생성됨을 댓글 작성자에게 알려야하는데 현재 사용자가 댓글 작성자가 아니라면
-        if (notifyCommentWriter) {
-            if (!determineCommentNoticeTarget(comment)) {
-                return;
+        // 답글이 생성됨을 댓글 작성자에게 알려야하는데 현재 사용자가 댓글 작성자가 맞다면
+        if (notifyCommentWriter && (comment.getParent() != null)) {
+            if (determineCommentNoticeTarget(comment)) {
+                dispatch(targetMemberId, notiCategory);
             }
         }
-            
-        if (notiCategory.equals(NotiCategory.BAD_TYPE_NOTIFICATION)) {
-            log.info("[NOTICE BAD TYPE] 잘못된 로그가 반환되어 알림 AOP를 종료합니다.");
-            return;
-        }
-
-
-        dispatch(targetMemberId, notiCategory);
     }
 
     private void dispatch(final Long targetMemberId, final NotiCategory notiCategory) {
@@ -81,9 +78,13 @@ public class NotificationDispatcher {
         }
     }
 
-    // ToDo 댓글작성자에게 알림 보낼지 여부
+    // ToDo FetchJoin
     private boolean determineCommentNoticeTarget(final Comment comment) {
-
+        // 댓글 작성자가 맞는지 필터링
+        Long parentCommentWriterId = comment.getParent().getMember().getId();
+        if (getCurrentMember().getId().equals(parentCommentWriterId)) {
+            return true;
+        }
         return false;
     }
 
