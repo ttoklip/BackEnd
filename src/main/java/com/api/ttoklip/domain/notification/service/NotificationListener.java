02439f3bef8060d8e348b2d29c7b692fba7a6dc0
toychip <dlawnsgud427@naver.com>
2024-04-23 16:05:02 +0900

    feat: 비동기로 처리하여 트랜잭션 별도 처리 (#126)
    알림 전송과 같은 트랜잭션을 분리하여 영향을 주지 않도록 변경

--- src/main/java/com/api/ttoklip/domain/notification/service/NotificationListener.java
+++ src/main/java/com/api/ttoklip/domain/notification/service/NotificationListener.java
@@ -4,6 +4,7 @@ import com.api.ttoklip.domain.common.comment.Comment;
 import com.api.ttoklip.domain.notification.entity.CommentCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,6 +15,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class NotificationListener {
     private final NotificationDispatcher notificationDispatcher;
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void processCommentNotification(CommentCreatedEvent event) {
         log.info("----- NotificationService.processCommentNotification call");
