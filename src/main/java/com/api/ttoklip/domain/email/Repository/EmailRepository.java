6f820f16ad814bd7d10e13fa7076933205400fe1
jgmoon <6813821@tukorea.ac.kr>
2024-06-27 01:26:23 +0900

    del: 미사용 import 삭제 (#125)

--- src/main/java/com/api/ttoklip/domain/email/Repository/EmailRepository.java
+++ src/main/java/com/api/ttoklip/domain/email/Repository/EmailRepository.java
@@ -2,7 +2,6 @@ package com.api.ttoklip.domain.email.Repository;
 
 import com.api.ttoklip.domain.email.entity.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
