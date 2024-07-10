3fc3d5ed32565392333d135211740e3f4a5c8261
toychip <dlawnsgud427@naver.com>
2024-07-10 23:50:31 +0900

    refactor: reformat code (#154)

--- src/main/java/com/api/ttoklip/domain/email/controller/EmailController.java
+++ src/main/java/com/api/ttoklip/domain/email/controller/EmailController.java
@@ -9,14 +9,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @Tag(name = "Email", description = "메일 인증 관련 API")
 @RestController
