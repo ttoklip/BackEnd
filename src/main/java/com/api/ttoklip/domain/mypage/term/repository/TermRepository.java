f4b95384215786247c680fa2f8b1219b4707ad0b
jgmoon <6813821@tukorea.ac.kr>
2024-07-18 05:22:44 +0900

    chore: repository로 이동 (#168)

--- src/main/java/com/api/ttoklip/domain/mypage/term/repository/TermRepository.java
+++ src/main/java/com/api/ttoklip/domain/mypage/term/repository/TermRepository.java
@@ -1,5 +1,6 @@
-package com.api.ttoklip.domain.mypage.term.domain;
+package com.api.ttoklip.domain.mypage.term.repository;
 
+import com.api.ttoklip.domain.mypage.term.domain.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TermRepository extends JpaRepository<Term, Long> {
