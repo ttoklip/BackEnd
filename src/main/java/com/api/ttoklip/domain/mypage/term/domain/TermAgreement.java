0e1e87e8567e14f33c9cab3f698d1c53eb5f164c
jgmoon <6813821@tukorea.ac.kr>
2024-07-18 05:21:25 +0900

    feat: TermAgreement, TermAgreementRepository 작성 (#168)

---
+++ src/main/java/com/api/ttoklip/domain/mypage/term/domain/TermAgreement.java
@@ -0,0 +1,33 @@
+package com.api.ttoklip.domain.mypage.term.domain;
+
+import com.api.ttoklip.domain.common.base.BaseEntity;
+import com.api.ttoklip.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TermAgreement extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private LocalDateTime agreedDate;
+
+    public static TermAgreement from(Member member) {
+        return TermAgreement.builder()
+                .member(member)
+                .agreedDate(LocalDateTime.now())
+                .build();
+    }
+}
---
+++ src/main/java/com/api/ttoklip/domain/mypage/term/repository/TermAgreementRepository.java
@@ -0,0 +1,7 @@
+package com.api.ttoklip.domain.mypage.term.repository;
+
+import com.api.ttoklip.domain.mypage.term.domain.TermAgreement;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TermAgreementRepository extends CrudRepository<TermAgreement, Long> {
+}
