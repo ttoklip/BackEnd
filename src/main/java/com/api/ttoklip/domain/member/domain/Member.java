223b80061ff72127ed9b0d90ddebc9c643349b75
jgmoon <6813821@tukorea.ac.kr>
2024-07-18 05:22:16 +0900

    feat: Member-TermAgreement 매핑 (#168)

--- src/main/java/com/api/ttoklip/domain/member/domain/Member.java
+++ src/main/java/com/api/ttoklip/domain/member/domain/Member.java
@@ -8,6 +8,7 @@ import com.api.ttoklip.domain.honeytip.post.domain.HoneyTip;
 import com.api.ttoklip.domain.honeytip.scrap.domain.HoneyTipScrap;
 import com.api.ttoklip.domain.member.editor.MemberEditor;
 import com.api.ttoklip.domain.member.editor.MemberEditor.MemberEditorBuilder;
+import com.api.ttoklip.domain.mypage.term.domain.TermAgreement;
 import com.api.ttoklip.domain.newsletter.like.entity.NewsletterLike;
 import com.api.ttoklip.domain.newsletter.post.domain.Newsletter;
 import com.api.ttoklip.domain.newsletter.scarp.entity.NewsletterScrap;
@@ -70,6 +71,9 @@ public class Member extends BaseEntity {
 
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "member", cascade = CascadeType.REMOVE)
     private Profile profile;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "member", cascade = CascadeType.REMOVE)
+    private TermAgreement termAgreement;
 
     @Builder.Default
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
