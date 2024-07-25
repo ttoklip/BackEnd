046921980ba8e51001405867bf75a92bc79b5c63
toychip <dlawnsgud427@naver.com>
2024-07-26 03:21:20 +0900

    cicd auto test10

--- src/main/java/com/api/ttoklip/global/config/FCMConfig.java
+++ src/main/java/com/api/ttoklip/global/config/FCMConfig.java
@@ -4,31 +4,35 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class FCMConfig {
 
     @Value("${FIREBASE_CONFIG}")
     private String firebaseConfig;
 
-    @PostConstruct
-    public void firebaseMessaging() throws IOException {
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(getCredential()))
-                .setProjectId("ttoklip")
-                .build();
+    private static final String PATH_NAME = "/app/config/firebase-config.json";
 
+    @PostConstruct
+    public void init() throws IOException {
+        byte[] decodedConfig = Base64.getDecoder().decode(firebaseConfig);
+
+        File configFile = new File(PATH_NAME);
+        try (FileOutputStream fos = new FileOutputStream(configFile)) {
+            fos.write(decodedConfig);
+        }
+
+        FileInputStream serviceAccount = new FileInputStream(configFile);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
         FirebaseApp.initializeApp(options);
     }
-
-    private InputStream getCredential() {
-        return new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
-    }
-
 }
