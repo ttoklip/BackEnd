32d35c156b0aef61342a8fb5e759b183d11fb2a8
toychip <dlawnsgud427@naver.com>
2024-07-26 03:32:19 +0900

    cicd auto test13

--- src/main/java/com/api/ttoklip/global/config/FCMConfig.java
+++ src/main/java/com/api/ttoklip/global/config/FCMConfig.java
@@ -4,32 +4,20 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FCMConfig {
 
-    @Value("${FIREBASE_CONFIG}")
-    private String firebaseConfig;
-
-    private static final String PATH_NAME = "/app/config/firebase-config.json";
+    @Value("${firebase.config.path:/app/config/firebase-config.json}")
+    private String firebaseConfigPath;
 
     @PostConstruct
     public void init() throws IOException {
-        byte[] decodedConfig = Base64.getDecoder().decode(firebaseConfig);
-
-        File configFile = new File(PATH_NAME);
-        try (FileOutputStream fos = new FileOutputStream(configFile)) {
-            fos.write(decodedConfig);
-        }
-
-        FileInputStream serviceAccount = new FileInputStream(configFile);
+        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
