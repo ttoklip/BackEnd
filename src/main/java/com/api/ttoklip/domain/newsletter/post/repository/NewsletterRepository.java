8f6f22c9209c8d25c977313dbe6a71c149d200e9
jgmoon <6813821@tukorea.ac.kr>
2024-09-04 20:09:37 +0900

    fix: 누락된 코드 추가 (#201)

--- src/main/java/com/api/ttoklip/domain/newsletter/post/repository/NewsletterRepository.java
+++ src/main/java/com/api/ttoklip/domain/newsletter/post/repository/NewsletterRepository.java
@@ -1,8 +1,10 @@
 package com.api.ttoklip.domain.newsletter.post.repository;
 
 import com.api.ttoklip.domain.newsletter.post.domain.Newsletter;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Primary
 public interface NewsletterRepository extends JpaRepository<Newsletter, Long>, NewsletterQueryDslRepository {
 
 }
