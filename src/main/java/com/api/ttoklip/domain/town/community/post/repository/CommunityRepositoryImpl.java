0eaf67ac6a50fcc3e5e8718daa3ead5d8b3fee25
toychip <dlawnsgud427@naver.com>
2024-09-22 16:45:09 +0900

    refactor: 우리동네 - 소통해요 주소 필터링 Filter 객체로 모듈화 (#216)

--- src/main/java/com/api/ttoklip/domain/town/community/post/repository/CommunityRepositoryImpl.java
+++ src/main/java/com/api/ttoklip/domain/town/community/post/repository/CommunityRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.api.ttoklip.domain.town.community.post.repository;
 
 import static com.api.ttoklip.domain.member.domain.QMember.member;
 import static com.api.ttoklip.domain.privacy.domain.QProfile.profile;
+import static com.api.ttoklip.domain.town.LocationCriteriaFilter.getLocationFilterByTownCriteria;
 import static com.api.ttoklip.domain.town.community.comment.QCommunityComment.communityComment;
 import static com.api.ttoklip.domain.town.community.image.entity.QCommunityImage.communityImage;
 import static com.api.ttoklip.domain.town.community.post.entity.QCommunity.community;
@@ -22,11 +23,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-
 @RequiredArgsConstructor
 public class CommunityRepositoryImpl implements CommunityRepositoryCustom {
 
-    private static final String SPLIT_CRITERIA = " ";
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
@@ -137,59 +136,6 @@ public class CommunityRepositoryImpl implements CommunityRepositoryCustom {
                         getLocationFilterByTownCriteria(townCriteria, writerStreet)
                 )
                 .fetchOne();
-    }
-
-    private BooleanExpression getLocationFilterByTownCriteria(final TownCriteria townCriteria, final String street) {
-        String[] streetParts = splitStreet(street);  // 공통 메서드로 분리
-
-        if (townCriteria.equals(TownCriteria.CITY)) {
-            return filterByCity(streetParts);
-        }
-
-        if (townCriteria.equals(TownCriteria.DISTRICT)) {
-            return filterByDistrict(streetParts);
-        }
-
-        if (townCriteria.equals(TownCriteria.TOWN)) {
-            return filterByTown(streetParts);
-        }
-
-        throw new ApiException(ErrorType.INTERNAL_STREET_TYPE);
-    }
-
-    // 주소를 공백으로 분리하는 로직을 하나의 메서드로 추출
-    private String[] splitStreet(final String street) {
-        return street.split(SPLIT_CRITERIA);
-    }
-
-    // '시' 부분만 추출해서 필터링 (예: '서울특별시'로 시작하는 모든 주소)
-    private BooleanExpression filterByCity(final String[] streetParts) {
-        if (streetParts.length > 0) {
-            String city = streetParts[0];
-            return community.member.street.startsWith(city);
-        }
-        return null;
-    }
-
-    // '시'와 '구'가 모두 일치해야 함 (예: '서울특별시 서대문구')
-    private BooleanExpression filterByDistrict(final String[] streetParts) {
-        if (streetParts.length > 1) {
-            String city = streetParts[0];
-            String district = streetParts[1];
-            return community.member.street.startsWith(city + SPLIT_CRITERIA + district);
-        }
-        return null;
-    }
-
-    // '시', '구', '동'이 모두 일치해야 함 (예: '서울특별시 서대문구 북가좌동')
-    private BooleanExpression filterByTown(final String[] streetParts) {
-        if (streetParts.length > 2) {
-            String city = streetParts[0];
-            String district = streetParts[1];
-            String town = streetParts[2];
-            return community.member.street.startsWith(city + SPLIT_CRITERIA + district + SPLIT_CRITERIA + town);
-        }
-        return null;
     }
 
 }
