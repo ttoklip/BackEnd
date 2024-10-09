b6ed0cffd3d3966192c3ea5adcc12f0c3c3f1cf7
toychip <dlawnsgud427@naver.com>
2024-10-09 19:10:39 +0900

    test: HoneyTipPostService test 뼈대 구성 (#238)

---
+++ src/test/java/com/api/ttoklip/domain/honeytip/service/HoneyTipPostServiceTest.java
@@ -0,0 +1,61 @@
+package com.api.ttoklip.domain.honeytip.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@ActiveProfiles("test")
+@SpringBootTest
+class HoneyTipPostServiceTest {
+
+    @Test
+    void getHoneytip() {
+    }
+
+    @Test
+    void uploadImages() {
+    }
+
+    @Test
+    void checkEditPermission() {
+    }
+
+    @Test
+    void saveHoneyTipPost() {
+    }
+
+    @Test
+    void findHoneyTipWithDetails() {
+    }
+
+    @Test
+    void findHouseworkTips() {
+    }
+
+    @Test
+    void findRecipeTips() {
+    }
+
+    @Test
+    void findSafeLivingTips() {
+    }
+
+    @Test
+    void findWelfarePolicyTips() {
+    }
+
+    @Test
+    void getPopularityTop5() {
+    }
+
+    @Test
+    void findRecent3() {
+    }
+
+    @Test
+    void matchCategoryPaging() {
+    }
+}
