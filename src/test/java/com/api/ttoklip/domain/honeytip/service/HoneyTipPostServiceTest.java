fa4ff783c4988c339cc4499a1b63d9eac4c8b108
toychip <dlawnsgud427@naver.com>
2024-10-10 22:02:13 +0900

    test: HoneyTipPost getPost 조회 테스트 (#238)

--- src/test/java/com/api/ttoklip/domain/honeytip/service/HoneyTipPostServiceTest.java
+++ src/test/java/com/api/ttoklip/domain/honeytip/service/HoneyTipPostServiceTest.java
@@ -1,22 +1,62 @@
 package com.api.ttoklip.domain.honeytip.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.api.ttoklip.domain.common.Category;
+import com.api.ttoklip.domain.honeytip.domain.HoneyTip;
+import com.api.ttoklip.domain.honeytip.repository.post.HoneyTipRepository;
+import com.api.ttoklip.global.config.QuerydslConfig;
+import com.api.ttoklip.global.exception.ApiException;
+import com.api.ttoklip.global.exception.ErrorType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(QuerydslConfig.class)
+@TestPropertySource(properties = "JASYPT_ENCRYPTOR_PASSWORD=dummy")
+@Sql("/sql/honeytip-reposiotry-test-data.sql")
 class HoneyTipPostServiceTest {
 
-    @Test
-    void getHoneytip() {
-    }
+    @Autowired
+    private HoneyTipPostService honeyTipPostService;
+
+    @Autowired
+    private HoneyTipRepository honeyTipRepository;
 
     @Test
-    void uploadImages() {
+    @DisplayName("존재하는 HoneyTip 조회 요청을 했을 때, 조회한 HoneyTip은 활성화 상태이다.")
+    void getHoneyTip() {
+        HoneyTip honeytip = honeyTipPostService.getHoneytip(900L);
+        assertThat(honeytip.isDeleted()).isFalse();
+        assertThat(honeytip.getId()).isEqualTo(900L);
+        assertThat(honeytip.getTitle()).isEqualTo("제목");
+        assertThat(honeytip.getContent()).isEqualTo("내용");
+        assertThat(honeytip.getCategory()).isEqualTo(Category.HOUSEWORK);
+    }
+
+
+    @Test
+    @DisplayName("존재하지 않는 HoneyTip 조회 요청을 했을 때, ApiException이 발생하고, 에러 타입이 HONEY_TIP_NOT_FOUND이다.")
+    void getHoneytipNotFound() {
+        // given
+        Long nonExistentHoneyTipId = 1001L;
+
+        // when & then
+        assertThatThrownBy(() -> honeyTipPostService.getHoneytip(nonExistentHoneyTipId))
+                .isInstanceOf(ApiException.class)
+                .satisfies(ex -> {
+                    ApiException apiException = (ApiException) ex;
+                    assertThat(apiException.getErrorType()).isEqualTo(ErrorType.HONEY_TIP_NOT_FOUND);
+                });
     }
 
     @Test
