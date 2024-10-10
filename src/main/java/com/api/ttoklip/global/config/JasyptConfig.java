0564a60b27d7075a2781d000d11e9797d571fc9c
toychip <dlawnsgud427@naver.com>
2024-10-10 21:58:28 +0900

    test: FcmConfig, JasyptConfig test가 아닌 프로필에만 동작하도록 설정 (#238)

--- src/main/java/com/api/ttoklip/global/config/JasyptConfig.java
+++ src/main/java/com/api/ttoklip/global/config/JasyptConfig.java
@@ -7,8 +7,10 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 @EnableEncryptableProperties
 public class JasyptConfig {
 
