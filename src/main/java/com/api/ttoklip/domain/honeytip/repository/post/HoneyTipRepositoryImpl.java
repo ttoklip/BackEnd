0c0673cfc2eea2ec44ef13f4f4acb0eb6041518e
toychip <dlawnsgud427@naver.com>
2024-10-11 15:59:30 +0900

    fix: 인기순 조회시 랭킹이 같다면 최신순으로 오도록 쿼리 수정 (#238)

--- src/main/java/com/api/ttoklip/domain/honeytip/repository/post/HoneyTipRepositoryImpl.java
+++ src/main/java/com/api/ttoklip/domain/honeytip/repository/post/HoneyTipRepositoryImpl.java
@@ -205,7 +205,8 @@ public class HoneyTipRepositoryImpl implements HoneyTipRepositoryCustom {
                         getLikeSize()
                                 .add(getCommentSize())
                                 .add(getScrapSize())
-                                .desc()
+                                .desc(),
+                        honeyTip.createdDate.desc()
                 )
                 .limit(5)
                 .fetch();
