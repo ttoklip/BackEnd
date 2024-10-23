c42354bf00f96af31055f8f3f566729c88d040e5
toychip <dlawnsgud427@naver.com>
2024-10-23 17:50:45 +0900

    fix: QuestionPost DI 활용 JPARepository 구현체에서 서택하도록 변경 (#234)

--- src/main/java/com/api/ttoklip/domain/question/post/repository/QuestionRepositoryImpl.java
+++ src/main/java/com/api/ttoklip/domain/question/post/repository/QuestionRepositoryImpl.java
@@ -1,196 +1,68 @@
 package com.api.ttoklip.domain.question.post.repository;
 
-import static com.api.ttoklip.domain.member.domain.QMember.member;
-import static com.api.ttoklip.domain.privacy.domain.QProfile.profile;
-import static com.api.ttoklip.domain.question.comment.domain.QQuestionComment.questionComment;
-import static com.api.ttoklip.domain.question.image.domain.QQuestionImage.questionImage;
-import static com.api.ttoklip.domain.question.post.domain.QQuestion.question;
-
 import com.api.ttoklip.domain.common.Category;
 import com.api.ttoklip.domain.question.comment.domain.QuestionComment;
 import com.api.ttoklip.domain.question.post.domain.Question;
-import com.api.ttoklip.global.exception.ApiException;
-import com.api.ttoklip.global.exception.ErrorType;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Wildcard;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RequiredArgsConstructor
-public class QuestionRepositoryImpl implements QuestionRepositoryCustom {
+public class QuestionRepositoryImpl implements QuestionRepository {
 
-    private final JPAQueryFactory jpaQueryFactory;
-
-    @Override
-    public QuestionComment findByCommentIdActivated(final Long commentId) {
-        QuestionComment findQuestionComment = jpaQueryFactory
-                .selectFrom(questionComment)
-                .distinct()
-                .leftJoin(questionComment.member, member).fetchJoin()
-                .where(
-                        matchCommentId(commentId)
-                )
-//                .fetchFirst();
-                .fetchOne();
-        return Optional.ofNullable(findQuestionComment)
-                .orElseThrow(() -> new ApiException(ErrorType.COMMENT_NOT_FOUND));
-    }
-
-    private BooleanExpression matchCommentId(final Long commentId) {
-        return questionComment.id.eq(commentId);
-    }
+    private final QuestionJpaRepository jpaRepository;
+    private final QuestionQueryRepository queryDslRepository;
 
     @Override
     public Question findByIdActivated(final Long questionId) {
-        Question findQuestion = jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .leftJoin(question.member, member).fetchJoin()
-                .where(
-                        matchId(questionId), getQuestionActivate()
-                )
-                .fetchOne();
-        return Optional.ofNullable(findQuestion)
-                .orElseThrow(() -> new ApiException(ErrorType.QUESTION_NOT_FOUND));
+        return queryDslRepository.findByIdActivated(questionId);
     }
 
-    private BooleanExpression matchId(final Long questionId) {
-        return question.id.eq(questionId);
-    }
-
-    private BooleanExpression getQuestionActivate() {
-        return question.deleted.isFalse();
+    @Override
+    public QuestionComment findByCommentIdActivated(final Long commentId) {
+        return queryDslRepository.findByCommentIdActivated(commentId);
     }
 
     @Override
     public Question findByIdFetchJoin(final Long questionPostId) {
-        Question findQuestion = jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .leftJoin(question.questionComments, questionComment)
-                .leftJoin(question.questionImages, questionImage)
-                .leftJoin(question.member, member).fetchJoin()
-                .leftJoin(question.member.profile, profile).fetchJoin()
-                .where(question.id.eq(questionPostId))
-                .fetchOne();
-
-        return Optional.ofNullable(findQuestion)
-                .orElseThrow(() -> new ApiException(ErrorType.QUESTION_NOT_FOUND));
+        return queryDslRepository.findByIdFetchJoin(questionPostId);
     }
 
     @Override
     public List<QuestionComment> findActiveCommentsByQuestionId(final Long questionId) {
-        return jpaQueryFactory
-                .selectFrom(questionComment)
-                .distinct()
-                .where(
-                        matchQuestionId(questionId)
-                )
-                .orderBy(
-                        questionComment.parent.id.asc().nullsFirst(),
-                        questionComment.createdDate.asc()
-                )
-                .fetch();
+        return queryDslRepository.findActiveCommentsByQuestionId(questionId);
     }
-
-    private BooleanExpression matchQuestionId(final Long questionId) {
-        return questionComment.question.id.eq(questionId);
-    }
-
-    // ------------------------------------ 메인 페이지 꿀팁공유해요 카테고리별 페이징 조회 ------------------------------------
 
     @Override
     public Page<Question> matchCategoryPaging(final Category category, final Pageable pageable) {
-        List<Question> pageContent = getPageContent(category, pageable);
-        Long count = pagingCountQuery(category);
-        return new PageImpl<>(pageContent, pageable, count);
+        return queryDslRepository.matchCategoryPaging(category, pageable);
     }
 
-    private List<Question> getPageContent(final Category category, final Pageable pageable) {
-        return jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .where(
-                        matchCategory(category)
-                )
-                .leftJoin(question.questionComments, questionComment)
-                .limit(pageable.getPageSize())
-                .offset(pageable.getOffset())
-                .orderBy(question.id.desc())
-                .fetch();
-    }
-
-    private BooleanExpression matchCategory(final Category category) {
-        return question.category.eq(category);
-    }
-
-    private Long pagingCountQuery(final Category category) {
-        return jpaQueryFactory
-                .select(Wildcard.count)
-                .from(question)
-                .where(
-                        matchCategory(category)
-                )
-                .fetchOne();
-    }
-
-    // ------------------------------------ 메인 페이지 꿀팁공유해요 카테고리별 페이징 조회 끝 ------------------------------------
-
+    @Override
     public List<Question> getHouseWork() {
-        return jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .leftJoin(question.questionComments, questionComment)
-                .leftJoin(question.member, member).fetchJoin()
-                .leftJoin(question.member.profile, profile).fetchJoin()
-                .where(question.category.eq(Category.HOUSEWORK))
-                .limit(10)
-                .orderBy(question.id.desc())
-                .fetch();
+        return queryDslRepository.getHouseWork();
     }
 
+    @Override
     public List<Question> getRecipe() {
-        return jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .leftJoin(question.questionComments, questionComment)
-                .leftJoin(question.member, member).fetchJoin()
-                .leftJoin(question.member.profile, profile).fetchJoin()
-                .where(question.category.eq(Category.RECIPE))
-                .limit(10)
-                .orderBy(question.id.desc())
-                .fetch();
+        return queryDslRepository.getRecipe();
     }
 
+    @Override
     public List<Question> getSafeLiving() {
-        return jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .leftJoin(question.questionComments, questionComment)
-                .leftJoin(question.member, member).fetchJoin()
-                .leftJoin(question.member.profile, profile).fetchJoin()
-                .where(question.category.eq(Category.SAFE_LIVING))
-                .limit(10)
-                .orderBy(question.id.desc())
-                .fetch();
+        return queryDslRepository.getSafeLiving();
     }
 
+    @Override
     public List<Question> getWelfarePolicy() {
-        return jpaQueryFactory
-                .selectFrom(question)
-                .distinct()
-                .leftJoin(question.questionComments, questionComment)
-                .leftJoin(question.member, member).fetchJoin()
-                .leftJoin(question.member.profile, profile).fetchJoin()
-                .where(question.category.eq(Category.WELFARE_POLICY))
-                .limit(10)
-                .orderBy(question.id.desc())
-                .fetch();
+        return queryDslRepository.getWelfarePolicy();
     }
 
+    @Override
+    public Question save(final Question question) {
+        return jpaRepository.save(question);
+    }
 }
