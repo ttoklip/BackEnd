a4e130bd860c7a99b143e9ef242903f06403d2ca
toychip <dlawnsgud427@naver.com>
2024-11-05 00:55:30 +0900

    refactor: NoticeRepository JPA에 의존하지 않게 DI 적용하여 구현체에서 선택하도록 변경 (#255) refactor: Notice Facade, Service 분리 (#255) feat: 공지사항 수정 관리자만 가능하도록 검증 추가 (#255) refactor: API Response Record로 변경 (#255)

--- src/main/java/com/api/ttoklip/domain/bulletin/dto/response/NoticeResponse.java
+++ src/main/java/com/api/ttoklip/domain/bulletin/dto/response/NoticeResponse.java
@@ -4,36 +4,21 @@ import com.api.ttoklip.domain.bulletin.domain.Notice;
 import com.api.ttoklip.global.util.TimeUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
-@AllArgsConstructor
-public class NoticeResponse {
-    @Schema(description = "공지사항 ID")
-    private Long noticeId;
-
-    @Schema(description = "공지사항 제목")
-    private String title;
-
-    @Schema(description = "공지사항 내용")
-    private String content;
-
-    @Schema(description = "공지사항 작성일자")
-    private String createdAt;
-
+public record NoticeResponse(
+        @Schema(description = "공지사항 ID") Long noticeId,
+        @Schema(description = "공지사항 제목") String title,
+        @Schema(description = "공지사항 내용") String content,
+        @Schema(description = "공지사항 작성일자") String createdAt
+) {
     public static NoticeResponse of(final Notice notice) {
-
         String formattedCreatedDate = getFormattedCreatedDate(notice);
-
-        return NoticeResponse.builder()
-                .noticeId(notice.getId())
-                .title(notice.getTitle())
-                .content(notice.getContent())
-                .createdAt(formattedCreatedDate)
-                .build();
+        return new NoticeResponse(
+                notice.getId(),
+                notice.getTitle(),
+                notice.getContent(),
+                formattedCreatedDate
+        );
     }
 
     private static String getFormattedCreatedDate(final Notice notice) {
