ec1b01436fa79d6b95d6d48003fea8469b7043de
toychip <dlawnsgud427@naver.com>
2024-11-07 10:18:43 +0900

    feat: Event Configuration (#264)

---
+++ ttoklip-common/src/main/java/com/common/config/event/EventsConfiguration.java
@@ -0,0 +1,19 @@
+package com.common.config.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class EventsConfiguration {
+
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public InitializingBean eventInitializer() {
+        return () -> Events.setPublisher(applicationContext);
+    }
+}
