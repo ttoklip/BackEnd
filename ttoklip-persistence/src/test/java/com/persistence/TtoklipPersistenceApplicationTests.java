7c29835636079fe36b9c16f8280d6458f85e0315
toychip <dlawnsgud427@naver.com>
2024-11-07 22:36:38 +0900

    setting: add persistence module (#264)

---
+++ ttoklip-persistence/src/test/java/com/persistence/TtoklipPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtoklipPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
