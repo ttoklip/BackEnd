7277e65659ef7293824257a89dac124384da7db3
toychip <dlawnsgud427@naver.com>
2024-11-18 22:22:16 +0900

    setting: 안쓰는 도커 이미지 정리 (#286)

--- ttoklip-api/src/main/java/com/api/TtoklipApiApplication.java
+++ ttoklip-api/src/main/java/com/api/TtoklipApiApplication.java
@@ -2,7 +2,6 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
