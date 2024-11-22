745b7b57baf51f15f699da7cfef79e5567602ba8
jgmoon <6813821@tukorea.ac.kr>
2024-11-22 11:22:40 +0900

    docs: Swagger 관련 코드만 Docs로 이동 (#269)
    LocalAuthController, LocalAuthControllerDocs

--- ttoklip-api/src/main/java/com/api/auth/local/presentation/LocalAuthController.java
+++ ttoklip-api/src/main/java/com/api/auth/local/presentation/LocalAuthController.java
@@ -5,6 +5,11 @@ import com.api.global.support.response.Message;
 import com.api.global.support.response.TtoklipResponse;
 import com.domain.term.response.TermSignUpResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,22 +19,26 @@ public class LocalAuthController implements LocalAuthControllerDocs {
     private final AuthFacade authFacade;
 
     @Override
+    @PostMapping(value = "/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public TtoklipResponse<Message> signup(LocalMemberWebCreate request) {
         return new TtoklipResponse<>(authFacade.signup(request));
     }
 
     @Override
+    @PostMapping("/duplicate")
     public TtoklipResponse<Message> duplicate(String newId) {
         return new TtoklipResponse<>(authFacade.duplicate(newId));
     }
 
     @Override
+    @PostMapping("/login")
     public TtoklipResponse<AuthLoginResponse> login(AuthLogin authLogin) {
         AuthLoginResponse response = authFacade.login(authLogin);
         return new TtoklipResponse<>(response);
     }
 
     @Override
+    @GetMapping("/agree")
     public TtoklipResponse<TermSignUpResponse> getTermSignUp() {
         TermSignUpResponse response = authFacade.getTermWhenSignUp();
         return new TtoklipResponse<>(response);
---
+++ ttoklip-api/src/main/java/com/api/auth/local/presentation/LocalAuthControllerDocs.java
@@ -0,0 +1,42 @@
+package com.api.auth.local.presentation;
+
+import com.api.global.support.response.Message;
+import com.api.global.support.response.TtoklipResponse;
+import com.domain.term.response.TermSignUpResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "Auth", description = "111회원가입/로그인 관련 API")
+@RequestMapping("/api/v1/auth")
+public interface LocalAuthControllerDocs {
+
+    @Operation(summary = "회원가입", description = "직접 회원가입을 진행합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "회원가입 성공")
+    })
+    TtoklipResponse<Message> signup(@Validated @ModelAttribute LocalMemberWebCreate request);
+
+    @Operation(summary = "중복확인", description = "아이디 중복확인을 진행합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "회원가입 성공")
+    })
+    TtoklipResponse<Message> duplicate(@RequestParam String newId);
+
+    @Operation(summary = "로그인", description = "직접 로그인을 진행합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "로그인 성공")
+    })
+    TtoklipResponse<AuthLoginResponse> login(@RequestBody AuthLogin authLogin);
+
+    @Operation(summary = "회원가입 전용 이용약관 조회", description = "회원가입 전 이용약관을 조회하는 API입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "이용약관 조회 성공")
+    })
+    TtoklipResponse<TermSignUpResponse> getTermSignUp();
+}
