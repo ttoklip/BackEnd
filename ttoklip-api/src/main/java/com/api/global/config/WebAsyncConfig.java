0250e42e71d38d084b7bb5a530a7b53c6e0ad8ba
toychip <dlawnsgud427@naver.com>
2024-11-23 23:59:40 +0900

    fix: Web 모듈 web excutor 사용하도록 변경, 공통 AsyncExceptionHandler 사용 추가

--- ttoklip-api/src/main/java/com/api/global/config/WebAsyncConfig.java
+++ ttoklip-api/src/main/java/com/api/global/config/WebAsyncConfig.java
@@ -1,24 +1,32 @@
 package com.api.global.config;
 
+import com.common.exception.AsyncExceptionHandler;
 import jakarta.annotation.PostConstruct;
+import java.util.concurrent.Executor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j
 @Configuration
-public class WebAsyncConfig {
+@RequiredArgsConstructor
+public class WebAsyncConfig implements AsyncConfigurer {
+
+    private final AsyncExceptionHandler asyncExceptionHandler;
 
     @PostConstruct
     public void initSecurityContextHolderStrategy() {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
-    @Bean
+    @Bean(name = "asyncExecutor")
     public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setCorePoolSize(5);
@@ -27,5 +35,17 @@ public class WebAsyncConfig {
         threadPoolTaskExecutor.setThreadNamePrefix("web-async-executor-");
         threadPoolTaskExecutor.initialize();
         return new DelegatingSecurityContextAsyncTaskExecutor(threadPoolTaskExecutor);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        // API 모듈의 TaskExecutor 사용
+        return taskExecutor();
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        // 공통 예외 핸들러 사용
+        return asyncExceptionHandler;
     }
 }
