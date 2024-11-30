afe7a87eac36fbfbae1933f8494ea43249a40048
toychip <dlawnsgud427@naver.com>
2024-11-30 22:17:37 +0900

    refactor: ErrorRootFinder Common으로 이동 (#293)

--- ttoklip-api/src/main/java/com/api/global/exception/ApiExceptionHandler.java
+++ ttoklip-api/src/main/java/com/api/global/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.api.global.exception;
 
 import com.common.config.event.Events;
+import com.common.event.ErrorRootFinder;
 import com.common.event.InternalServerErrorEvent;
 import com.common.event.Modules;
 import com.common.exception.ApiException;
@@ -8,7 +9,6 @@ import com.common.exception.BadWordException;
 import com.common.exception.ErrorType;
 import com.common.exception.HttpStatusCode;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,9 +20,9 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler({ApiException.class})
     public ResponseEntity<ApiExceptionResponse> handleApiException(final ApiException e) {
-        Modules modules = determineModuleFromException(e);
+        Modules modules = ErrorRootFinder.determineModuleFromException(e);
         if (e.getErrorType().getStatus() == HttpStatusCode.INTERNAL_SERVER_ERROR) {
-            sendDiscord(e, modules);
+            alertErrorEvent(e, modules);
         }
 
         ErrorType errorType = e.getErrorType();
@@ -34,15 +34,15 @@ public class ApiExceptionHandler {
         return ResponseEntity.status(errorType.getStatus()).body(response);
     }
 
-    private void sendDiscord(final Throwable e, Modules module) {
+    private void alertErrorEvent(final Throwable e, Modules module) {
         log.error("Unhandled exception occurred", e);
         Events.raise(new InternalServerErrorEvent(LocalDateTime.now(), e, module));
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<ApiExceptionResponse> handleInternalException(final Exception e) {
-        Modules modules = determineModuleFromException(e);
-        sendDiscord(e, modules);
+        Modules modules = ErrorRootFinder.determineModuleFromException(e);
+        alertErrorEvent(e, modules);
 
         String INTERNAL_ERROR_CODE = "TTOKLIP_COMMON_INTERNAL_SERVER_ERROR";
         String INTERNAL_ERROR_MESSAGE = "알 수 없는 에러입니다. 관리자에게 문의하세요.";
@@ -63,39 +63,5 @@ public class ApiExceptionHandler {
                 e.getMessage()
         );
         return ResponseEntity.status(e.getStatus()).body(response);
-    }
-
-    private Modules determineModuleFromException(Throwable e) {
-        Throwable rootCause = getRootCause(e);
-
-        return Arrays.stream(rootCause.getStackTrace())
-                .map(StackTraceElement::getClassName)
-                .map(this::getModuleFromClassName)
-                .filter(module -> module != Modules.UNKNOWN)
-                .findFirst()
-                .orElse(Modules.UNKNOWN);
-    }
-
-    private Throwable getRootCause(Throwable e) {
-        Throwable cause = e.getCause();
-        if (cause != null && cause != e) {
-            return getRootCause(cause);
-        } else {
-            return e;
-        }
-    }
-
-    private Modules getModuleFromClassName(String className) {
-        if (className.startsWith(Modules.API.getRoot())) {
-            return Modules.API;
-        } else if (className.startsWith(Modules.DOMAIN.getRoot())) {
-            return Modules.DOMAIN;
-        } else if (className.startsWith(Modules.INFRASTRUCTURE.getRoot())) {
-            return Modules.INFRASTRUCTURE;
-        } else if (className.startsWith(Modules.COMMON.getRoot())) {
-            return Modules.COMMON;
-        } else {
-            return Modules.UNKNOWN;
-        }
     }
 }
