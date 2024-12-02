3a4f55ad6db64d3324744d4d87cd702766cf2bcf
toychip <dlawnsgud427@naver.com>
2024-12-02 15:31:30 +0900

    refactor: foreach refactirng

--- ttoklip-common/src/main/java/com/common/exception/AsyncExceptionPublisher.java
+++ ttoklip-common/src/main/java/com/common/exception/AsyncExceptionPublisher.java
@@ -4,6 +4,7 @@ import com.common.config.event.Events;
 import com.common.event.AsyncInternalServerExceptionEvent;
 import com.common.event.Modules;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,10 @@ public class AsyncExceptionPublisher {
         Modules module = ErrorRootFinder.determineModuleFromException(ex);
 
         log.error("Unexpected exception occurred in async method: {}", methodName, ex);
-        for (Object param : params) {
-            log.error("Parameter value - {}", param);
-        }
+
+        Arrays.stream(params)
+                .forEach(param -> log.error("Parameter value - {}", param));
+
         Events.raise(new AsyncInternalServerExceptionEvent(LocalDateTime.now(), ex, module));
     }
 }
