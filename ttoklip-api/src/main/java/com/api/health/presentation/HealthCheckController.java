d72e32297ece0651550b68b53d57430936e94944
jgmoon <6813821@tukorea.ac.kr>
2024-12-22 23:03:28 +0900

    fix: 미사용 코드 삭제 (#303)

--- ttoklip-api/src/main/java/com/api/health/presentation/HealthCheckController.java
+++ ttoklip-api/src/main/java/com/api/health/presentation/HealthCheckController.java
@@ -3,12 +3,10 @@ package com.api.health.presentation;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Hidden
 @RestController
-@RequestMapping
 public class HealthCheckController {
 
     @GetMapping("/health")
