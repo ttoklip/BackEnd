efb5d7326ea99a5b976237318653c002cf2a9282
toychip <dlawnsgud427@naver.com>
2025-01-09 19:20:55 +0900

    fix: MemberRepository.save() saveAndFlush -> save로 변경. 락 해제

--- ttoklip-domain/src/main/java/com/domain/member/infrastructure/MemberRepositoryImpl.java
+++ ttoklip-domain/src/main/java/com/domain/member/infrastructure/MemberRepositoryImpl.java
@@ -51,7 +51,7 @@ public class MemberRepositoryImpl implements MemberRepository {
 
     @Override
     public void save(final Member member) {
-        jpaRepository.saveAndFlush(member);
+        jpaRepository.save(member);
     }
 
     @Override
